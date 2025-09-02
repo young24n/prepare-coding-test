4ddb734635e7ba1876f2d7cc5a8099fb76b95cff
young24n <won8560@gmail.com>
2025-09-02 09:31:43 +0900

    두 수의 차 구하기

---
+++ 프로그래머스/코딩테스트 입문/두 수의 차 구하기.js
@@ -0,0 +1,19 @@
+// 입문 문제는 너무 쉽다. 그렇기에 초반에는 하루 할당량을 늘려보겠다.
+
+// 두 수의 합 구하기와 같다. 문제 설명을 요약.
+// 문제 설명
+// 정수 num1과 num2가 주어질 때, num1과 num2의 차를 return하도록 soltuion 함수를 완성해주세요.
+
+/**  
+기본으로 제공되는 코드
+function solution(num1, num2) {
+    var answer = 0;
+    return answer;
+}
+*/
+
+function solution(num1, num2) {
+    var answer = num1 - num2
+    return answer;
+}
+
