260848c407cd9b04e01373b48199ed8772684294
young24n <won8560@gmail.com>
2025-09-02 09:31:29 +0900

    README 추가 프로그래머스 - 두 수의 합 구하기

---
+++ 프로그래머스/코딩테스트 입문/두 수의 합 구하기.js
@@ -0,0 +1,27 @@
+// 입문 문제는 너무 쉽다. 그렇기에 초반에는 하루 할당량을 늘려보겠다.
+
+// 문제 설명
+// 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+// 제한사항
+// -50,000 ≤ num1 ≤ 50,000
+// -50,000 ≤ num2 ≤ 50,000
+
+// 입출력 예
+// num1	num2 result
+// 2	3	 5
+// 100	2	 102
+
+/**  
+기본으로 제공되는 코드
+function solution(num1, num2) {
+    var answer = -1;
+    return answer;
+}
+*/
+
+function solution(num1, num2) {
+    var answer = num1 + num2
+    return answer;
+}
+
