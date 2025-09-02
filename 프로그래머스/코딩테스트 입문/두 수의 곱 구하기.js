5848119f279ca1320b94cce076632ad67acc5201
young24n <won8560@gmail.com>
2025-09-02 09:31:52 +0900

    두 수의 곱 구하기

---
+++ 프로그래머스/코딩테스트 입문/두 수의 곱 구하기.js
@@ -0,0 +1,18 @@
+// 두 수의 합 구하기와 같다. 문제 설명을 축약.
+// 문제 설명
+
+// 정수 num1, num2가 매개변수 주어집니다. 
+// num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+/**  
+기본으로 제공되는 코드
+function solution(num1, num2) {
+    var answer = 0;
+    return answer;
+}
+*/
+
+function solution(num1, num2) {
+    var answer = num1 * num2;
+    return answer;
+}
