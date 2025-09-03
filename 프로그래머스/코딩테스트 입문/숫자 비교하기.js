cb48c124d9edbb49369c7381d33c0d09424bafbe
young24n <won8560@gmail.com>
2025-09-03 18:42:31 +0900

    숫자 비교하기

---
+++ 프로그래머스/코딩테스트 입문/숫자 비교하기.js
@@ -0,0 +1,19 @@
+// 숫자 비교하기
+
+// 문제 내용
+// 정수 num1과 num2가 매개변수로 주어집니다. 
+// 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+
+// function solution(num1, num2) {
+//     var answer = 0;
+//     return answer;
+// }
+
+// 설명 생략
+
+function solution(num1, num2) {
+    var answer = -1
+    if (num1 === num2) answer = 1
+
+    return answer;
+}
