f58e434f969e8af7569ab863988eb4dab80e4c86
young24n <won8560@gmail.com>
2025-09-05 11:19:42 +0900

    나머지 구하기

---
+++ 프로그래머스/코딩테스트 입문/나머지 구하기.js
@@ -0,0 +1,9 @@
+// 문제 설명
+// 정수 num1, num2가 매개변수로 주어질 때,
+//  num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+
+// 분수구하기를 먼저 풀게하고 이걸 풀게 하다니..
+function solution(num1, num2) {
+    var answer = num1 % num2
+    return answer;
+}
