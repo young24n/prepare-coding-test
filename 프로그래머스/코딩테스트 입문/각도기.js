2a67bb10d38c66394ee3c4b9538acee24d14c04d
young24n <won8560@gmail.com>
2025-09-11 23:16:38 +0900

    각도기

---
+++ 프로그래머스/코딩테스트 입문/각도기.js
@@ -0,0 +1,20 @@
+// 문제 설명
+// 각에서 0도 초과 90도 미만은 예각, 90도는 직각, 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다. 
+// 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
+
+// 예각 : 0 < angle < 90
+// 직각 : angle = 90
+// 둔각 : 90 < angle < 180
+// 평각 : angle = 180
+
+// 고맙게도 문제 설명에 조건이 모두 적혀있다. 이용해서 조건문을 만들면된다.
+
+function solution(angle) {
+    var answer = 0
+    if (0 < angle && angle < 90) answer = 1
+    else if (angle == 90) answer = 2
+    else if (90 < angle && angle < 180) answer = 3
+    else answer = 4
+
+    return answer;
+}
